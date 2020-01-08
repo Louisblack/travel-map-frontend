717ee7b62170bd9894b4971529f96fc1068032b2
Louis Houghton <louis@louishoughton.com>
2020-01-08 21:09:46 +0000

    Add chrome dependency

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 module "hosting" {
-  source             = "git::https://github.com/cloudposse/terraform-aws-cloudfront-cdn.git?ref=0.7.0"
+  source             = "git::https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn.git?ref=0.11.0"
   namespace          = var.namespace
   stage              = var.stage
   name               = "travel-map"
