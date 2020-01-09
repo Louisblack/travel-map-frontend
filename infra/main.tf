6291831959a541a52511676134125ea54b4bc260
Louis Houghton <louis@louishoughton.com>
2020-01-09 20:38:06 +0000

    Update infra action

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 module "hosting" {
-  source             = "git::https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn.git?ref=0.11.0"
+  source             = "./hosting"
   namespace          = var.namespace
   stage              = var.stage
   name               = "travel-map"
